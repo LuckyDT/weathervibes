7aba2c354b765c2cc89199ffa2ca3569c57f9562
@LuckyDT <dentimofeev@gmail.com>
2026-02-25 10:38:34 +0300

    Fix main: some flake errors were fixed

--- src/main.py
+++ src/main.py
@@ -1,16 +1,18 @@
 from rich import print
 from weather_api import get_weather
 
+
 def main():
     city = input("Введите город: ")
     weather = get_weather(city)
-    
+
     temp = weather["main"]["temp"]
     desc = weather["weather"][0]["description"]
     print(f"[bold cyan]Weather Vibes[/bold cyan]")
     print(f"City: [green]{city}[/green]")
     print(f"Temperature: [yellow]{temp}°C[/yellow]")
     print(f"Description: [magenta]{desc}[/magenta]")
+
 
 if __name__ == "__main__":
     main()
